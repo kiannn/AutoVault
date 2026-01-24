4fe742272c943c0c12efd9955313f2c201a3fa29
kiannn <kian1986@live.com>
2026-01-24 02:28:35 -0500

    spring.jpa.hibernate.ddl-auto set to validate, tables created manually using SQL commnad, userSessionValidity method removed

--- src/main/java/com/example/CarDealerShip/CarDealerShipApplication.java
+++ src/main/java/com/example/CarDealerShip/CarDealerShipApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
